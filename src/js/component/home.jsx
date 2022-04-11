1883aebc44e1cff5252cd69e95c103fac52cfa8c
jsoares07 <jfernandosoares7@gmail.com>
2022-04-11 19:41:45 +0000

    Added the frist part of the code

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,24 +1,42 @@
-import React from "react";
-
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+import React, { useState } from "react";
 
 //create your first component
 const Home = () => {
+	const [color, setColor] = useState("");
+
+	function clickRed() {
+		setColor("red");
+	}
+	function clickYellow() {
+		setColor("yellow");
+	}
+	function clickGreen() {
+		setColor("green");
+	}
+
 	return (
 		<div>
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+			<div className="container">
+				<h1>Traffic light with React</h1>
+				<button
+					className={
+						"rounded-circle red " +
+						(color === "red" ? "selected  blink" : "")
+					} //Curly braces are to inform react that we are going to add some expression to the Class
+					onClick={clickRed}></button>
+				<button
+					className={
+						"rounded-circle yellow " +
+						(color === "yellow" ? "selected blink" : "")
+					}
+					onClick={clickYellow}></button>
+				<button
+					className={
+						"rounded-circle green " +
+						(color === "green" ? "selected blink" : "")
+					}
+					onClick={clickGreen}></button>
+			</div>
 		</div>
 	);
 };
